0f451fb4889459a07037179fbe0291bd2708a77c
narayanraonl <62981865+narayanraonl@users.noreply.github.com>
2024-05-27 12:42:14 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {NavBasicExample} from './components/SideNav'
+import { clarity } from 'react-microsoft-clarity';
 import './App.css';
 
 export const App: React.FunctionComponent = () => {
+  useEffect(() => {
+    clarity.init('mie9m7he5y');
+  }, []);
   return (
+    
     <>
       <NavBasicExample />
     </>
